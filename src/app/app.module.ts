29b29d2f0046ef7d3967c8bc3207cbdb1a05072d
Edoardo Campagnano <edoardocampagnano@MacBook-Pro-di-Edoardo-3.local>
2019-09-08 19:52:51 +0200

    Struttura completa, to the services!!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,32 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
 
 import { SessionService } from './session.service';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MenuComponent } from './menu/menu.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    MenuComponent,
+    TransactionsComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: DashboardComponent },
+      { path: 'trxs', component: TransactionsComponent },
+
+    ])
   ],
   providers: [SessionService],
   bootstrap: [AppComponent]
