a47a2204d842250893e5566d95849bf7f86e7c14
Edoardo C <registrazioni.edoardo@gmail.com>
2019-10-11 10:18:52 +0200

    userDetails

--- src/app/domain/user.ts
+++ src/app/domain/user.ts
@@ -1,9 +1,12 @@
 export class User {
     id: number;
+    code: string;
     username: string;
     password: string;
     firstName: string;
     lastName: string;
+    companyCode: string;
+    companyName: string;
     // role: Role;
     token?: string;
 }
